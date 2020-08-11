f7f38f8e549aea6fd7c5cf0cecd4da8b0d41115d
seungeonjoo <wntmddjs0@gmail.com>
2020-08-11 18:23:45 +0900

    .

--- src/component/list.js
+++ src/component/list.js
@@ -7,41 +7,37 @@ class List extends React.Component {
         return (
             <>
             <div style={{padding:'32px'}}>
-                <ListText/>
+                <ListText id={0}/>
                 <div style={{overflow:'hidden',overflowX:'auto', marginTop:'10px'}}>
                     <div className="card-list" >
-
                         {data.map((contact, i) => {
                         return (
                         <ListItem index={i} data={contact}></ListItem>
                             )
-                        })};
+                        })}
                     </div>
                 </div>
             </div>
             <div style={{padding:'32px'}}>
-                <ListText/>
+                <ListText id={1}/>
                 <div style={{overflow:'hidden',overflowX:'auto', marginTop:'10px'}}>
                     <div className="card-list" >
-
                         {data.map((contact, i) => {
                             return (
                                 <ListItem index={i} data={contact}></ListItem>
                             )
-                        })};
+                        })}
                     </div>
                 </div>
             </div>
             <div style={{padding:'32px'}}>
-                <ListText/>
                 <div style={{overflow:'hidden',overflowX:'auto', marginTop:'10px'}}>
                     <div className="card-list" >
-
                         {data.map((contact, i) => {
                             return (
                                 <ListItem index={i} data={contact}></ListItem>
                             )
-                        })};
+                        })}
                     </div>
                 </div>
             </div>
