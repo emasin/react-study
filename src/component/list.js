a45b254e9d051971a2a7d05cf619128a00ede417
seungeonjoo <wntmddjs0@gmail.com>
2020-08-11 16:54:55 +0900

    .

--- src/component/list.js
+++ src/component/list.js
@@ -1,38 +1,51 @@
 import React from "react";
-
+import ListText from "../component/list-text";
 import ListItem from "./list-item";
 class List extends React.Component {
     render() {
         const data = this.props.data;
         return (
             <>
-            <div style={{overflow:'hidden',overflowX:'auto'}}>
-            <div className="card-list" >
+            <div style={{padding:'32px'}}>
+                <ListText/>
+                <div style={{overflow:'hidden',overflowX:'auto', marginTop:'10px'}}>
+                    <div className="card-list" >
 
-                {data.map((contact, i) => {
-                  return (
-                   <ListItem index={i} data={contact}></ListItem>
-                      )
-                })};
-            </div>
-
-
-            </div>
-
-
-        <div style={{overflow:'hidden',overflowX:'auto'}}>
-            <div className="card-list" >
-
-                {data.map((contact, i) => {
-                    return (
+                        {data.map((contact, i) => {
+                        return (
                         <ListItem index={i} data={contact}></ListItem>
-                    )
-                })};
+                            )
+                        })};
+                    </div>
+                </div>
             </div>
+            <div style={{padding:'32px'}}>
+                <ListText/>
+                <div style={{overflow:'hidden',overflowX:'auto', marginTop:'10px'}}>
+                    <div className="card-list" >
 
+                        {data.map((contact, i) => {
+                            return (
+                                <ListItem index={i} data={contact}></ListItem>
+                            )
+                        })};
+                    </div>
+                </div>
+            </div>
+            <div style={{padding:'32px'}}>
+                <ListText/>
+                <div style={{overflow:'hidden',overflowX:'auto', marginTop:'10px'}}>
+                    <div className="card-list" >
 
-        </div>
-                </>
+                        {data.map((contact, i) => {
+                            return (
+                                <ListItem index={i} data={contact}></ListItem>
+                            )
+                        })};
+                    </div>
+                </div>
+            </div>
+            </>
         )
     }
 }
