001f09ca2948dff748818a4bc84afa18b52e1765
seungeonjoo <wntmddjs0@gmail.com>
2020-08-27 15:24:31 +0900

    프로필 수정

--- src/component/profile.js
+++ src/component/profile.js
@@ -1,21 +1,35 @@
 import React from "react"
 import {useSelector} from "react-redux";
 import {Redirect} from "react-router-dom";
+import { Container } from "@material-ui/core";
+import styles from "../css/style.module.css";
 
-function Profile({ user }) {
-
+function Profile({ user } ) {
 
     const _user = useSelector(state => state.topics.user, []) || {};
 
     const { email, password, name } = user || {}
 
     const { from } =   { from:{pathname: "/login" }}
+    
     if (!_user.username) return <Redirect to={from} />
+
     return (
         <>
-            <h1>Profile</h1>
-            <dt>Email</dt>
-            <dd>{_user.username}</dd>
+            <Container style={{marginTop:'50px', marginBottom:'60px'}}>
+                    <h1 className={styles.title}>Profile</h1>
+                    <div className={styles.profilelist}>
+                        <dl>
+                            <dt>Email</dt>
+                            <dd>wntmddjs0@naver.com</dd>
+                        </dl>
+                        <hr/>
+                        <dl>
+                            <dt>Name</dt>
+                            <dd>{_user.username}</dd>
+                        </dl>
+                    </div>
+            </Container>
         </>
     )
 }
